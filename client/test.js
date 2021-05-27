eefaed7e9959a57dc963a0a4c6f96f088bcb0f20
Ken Litton <KennethCLitton@gmail.com>
2021-05-27 12:25:45 -0500

    installed supertest

--- client/test.js
+++ client/test.js
@@ -2,23 +2,25 @@ import Jest from 'jest';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import EnzymeToJson from 'enzyme-to-json';
 import ItineraryCreator from './components/ItineraryCreator.jsx';
 import UserContainer from './Containers/UserContainer.jsx';
 
 configure({adapter: new Adapter()});
 
-// describe('User Container tests', () => {
-//   let wrapper;
+describe('User Container tests', () => {
+  let wrapper;
 
-//   beforeAll(() => {
-//     wrapper = shallow(<UserContainer />)
-//   });
+  beforeAll(() => {
+    wrapper = shallow(<UserContainer />)
+  });
 
-//   it('User Creator should render a form element with the correct input fields', () => {
-//     expect(wrapper.find('input').simulate('change', {target: {value: 'My new value'}})
-//   })
-// })
+  it('User Container should render a form element with the correct input fields', () => {
+    expect(wrapper.find('h1').text()).toMatch('')
+  }
+  )
+})
 
 describe('Itinerary Creator tests', () => {
   let wrapper;
